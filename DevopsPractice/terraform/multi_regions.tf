90b7d5c07903ea9695c15fdffeadeb0d139324ce
Onyinye P <eyon4g@gmail.com>
2022-02-11 05:18:51 -0600

    security practices in devops

---
+++ DevopsPractice/terraform/multi_regions.tf
@@ -0,0 +1,17 @@
+provider "oci" {
+  alias            = "home"
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
+provider "oci" {
+  alias            = "region2"
+  region           = var.region2
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
